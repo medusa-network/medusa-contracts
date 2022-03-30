7b1b7d052fd0443185bed619b727583abd6da4db
nikkolasg <nikkolasg@gmail.com>
2022-03-30 13:41:59 +0100

    deploying only test contract

--- src/TestContract.sol
+++ src/TestContract.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.10;
 
-contract TestContract {
+import "./EncryptionOracle.sol";
+
+contract TestContract is EncryptionOracle {
     uint256 private value;
     function setValue(uint256 _value) public {
         value = _value;
