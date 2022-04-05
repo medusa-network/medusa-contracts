8c0d052e833d477ddae9341679558407f28bb483
nikkolasg <nikkolasg@gmail.com>
2022-04-05 11:04:21 +0100

    tests compiling

--- src/EncryptionOracle.sol
+++ src/EncryptionOracle.sol
@@ -4,13 +4,15 @@ pragma solidity >=0.8.10;
 import "./DkgManager.sol";
 
 interface IEncryptionClient {
-    function isAuthorized(uint256 id, uint256 publickey, uint256[] memory extra) external returns (bool);
-    function oracleResult(uint256 id, uint256 r, uint256 cipher, uint256 publickey) external ;
+    function isAuthorized(uint256 cipher_id, uint256 publickey, uint256[] memory extra) external returns (bool);
+    function oracleResult(uint256 cipher_id, uint256 request_id, uint256 r, uint256 cipher, uint256 publickey) external ;
 }
 
 interface IEncryptionOracle {
-    function requestReencryption(uint256 id, uint256 publickey) external; 
-    function submitCiphertext(uint256 r, uint256 cipher, uint256[] memory extra) external returns (uint256);
+    // returns the request id
+    function requestReencryption(uint256 _cipher_id, uint256 _publickey) external returns (uint256); 
+    // returns the ciphertext id
+    function submitCiphertext(uint256 _r, uint256 _cipher, uint256[] memory _extra) external returns (uint256);
 }
 
 contract EncryptionOracle is DKGManager, IEncryptionOracle {
@@ -23,18 +25,22 @@ contract EncryptionOracle is DKGManager, IEncryptionOracle {
     struct PendingRequest {
         address client;
         uint256 publickey;
+        uint256 cipher_id;
     }
     // pending request is used to track the reencryption requests, to make sure
     // the medusa node callsback the same contract that submitted the request in
     // the first place.
     mapping(uint256 => PendingRequest) pending_requests;
-    uint256 private nonce_id = 0;
+    // counter to derive unique nonces for each ciphertext ever submitted to the oracle
+    uint256 private cipher_nonce = 0;
+    // counter to derive unique nonces for each reencryption request ever submitted to the oracle
+    uint256 private request_nonce = 0;
 
     // id: unique identifier of the ciphertext
     // publickey: recipient for which we wish to reencrypt for
     // client: inserted such that medusa nodes check the client that originated
     // the newciphertext event, is the same that request the reencryptionrequest
-    event ReencryptionRequest(uint256 indexed id, uint256 publickey, address client);
+    event ReencryptionRequest(uint256 indexed cipher_id, uint256 request_id, uint256 publickey, address client);
     
     // id: newly created id for this ciphertext
     // r,cipher: random part of the ciphertext and then the "hashed"/encryption part of it
@@ -44,29 +50,28 @@ contract EncryptionOracle is DKGManager, IEncryptionOracle {
     // client.isAuthorized(.... extra) returns true.
     event NewCiphertext(uint256 indexed id, uint256 r, uint256 cipher, address client, uint256[] extra);
 
-    function newId() private returns (uint256) {
-        nonce_id += 1;
-        return nonce_id;
+    function newCipherId() private returns (uint256) {
+        cipher_nonce += 1;
+        return cipher_nonce;
+    }
+
+    function newRequestId() private returns (uint256) {
+        request_nonce += 1;
+        return request_nonce;
     }
     
-    function submitCiphertext(uint256 r, uint256 cipher, uint256[] memory extra) external returns (uint256) { 
-        uint256 id = newId();
-        emit NewCiphertext(id, r, cipher, msg.sender, extra);
+    function submitCiphertext(uint256 _r, uint256 _cipher, uint256[] memory _extra) external returns (uint256) { 
+        uint256 id = newCipherId();
+        emit NewCiphertext(id, _r, _cipher, msg.sender, _extra);
         return id;
     }
 
     // TODO payable etc
-    function requestReencryption(uint256 id, uint256 publickey) public {
-        // TODO that prevents having multiple requests for same id -> make a
-        // linked list
-        require(requestDoNotExists(id),"requests already exists");
-        pending_requests[id] = PendingRequest(msg.sender, publickey);
-        emit ReencryptionRequest(id, publickey, msg.sender);
-    }
-
-    function requestDoNotExists(uint256 id) private view returns (bool) {
-        PendingRequest memory pr = pending_requests[id];
-        return pr.client == address(0) && pr.publickey == 0;
+    function requestReencryption(uint256 _cipher_id, uint256 _publickey) public returns (uint256) {
+        uint256 request_id = newRequestId();
+        pending_requests[request_id] = PendingRequest(msg.sender, _publickey, _cipher_id);
+        emit ReencryptionRequest(_cipher_id, request_id, _publickey, msg.sender);
+        return request_id;
     }
 
     function requestDoExists(uint256 id) private view returns (bool) {
@@ -78,13 +83,13 @@ contract EncryptionOracle is DKGManager, IEncryptionOracle {
     // change, although we probably dont want to store it onchain? but then we
     // can't guarantee it's for the same, we have to "trust" the oracle  -- 
     // Check with zkproofs if they are sufficient to guarantee this
-    function deliverReencryption(uint256 id, uint256 r, uint256 cipher) public {
+    function deliverReencryption(uint256 _request_id, uint256 r, uint256 cipher) public {
         // TODO
         // 1. check that sender is authorized
-        require(requestDoExists(id));
-        PendingRequest memory pr = pending_requests[id];
-        delete(pending_requests[id]);
+        require(requestDoExists(_request_id));
+        PendingRequest memory pr = pending_requests[_request_id];
+        delete(pending_requests[_request_id]);
         IEncryptionClient client = IEncryptionClient(pr.client);
-        client.oracleResult(id, r, cipher, pr.publickey);
+        client.oracleResult(pr.cipher_id, pr.cipher_id, r, cipher, pr.publickey);
     }
 }
