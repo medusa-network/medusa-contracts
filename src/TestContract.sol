e1275accdbcb676d0d61efa6c37c599367ef71cb
nikkolasg <nikkolasg@gmail.com>
2022-05-02 22:51:18 +0100

    wip

--- src/TestContract.sol
+++ src/TestContract.sol
@@ -3,9 +3,20 @@ pragma solidity >=0.8.10;
 
 import "./EncryptionOracle.sol";
 import "./Bn128.sol";
+/*import "./altbn128.sol";*/
 
 contract TestContract is EncryptionOracle {
     uint private nonce;
+
+    Bn128.G1Point private accumulator;
+    /*altbn128.G1Point private acc2;*/
+
+    constructor() EncryptionOracle() {
+        accumulator = Bn128.g1Zero();
+        /*acc2 = altbn128.P1();*/
+        /*acc2.X = 0;*/
+        /*acc2.y = 0;*/
+    }
 
     // used to quickly setup a dist key without going through the whole DKG
     // onchain
@@ -14,5 +25,23 @@ contract TestContract is EncryptionOracle {
         Bn128.G1Point memory key = Bn128.g1Decompress(bytes32(_point));
         dist_key = key;
         nonce = block.number;
+    }
+
+    function addAccumulatorCompressed(uint256 _x) public {
+        Bn128.G1Point memory point = Bn128.g1Decompress(bytes32(_x));
+        addAccumulator(point);
+    }
+
+    function addAccumulator(Bn128.G1Point memory point) public {
+        /*Bn128.G1Point memory point = G1Point(_x,_y);*/
+        accumulator = Bn128.g1Add(accumulator,point);
+    }
+
+    function getAccumulator() public view returns (Bn128.G1Point memory) {
+        return accumulator;
+    }
+
+    function getAccumulatorCompressed() public view returns (uint256) {
+        return uint256(Bn128.g1Compress(getAccumulator()));
     }
 }
