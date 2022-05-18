d533bab3b8f7ac9ef7f2aa8d4418dea5696f674a
nikkolasg <nikkolasg@gmail.com>
2022-05-18 15:00:50 +0100

    using types

--- integration/mover.test.ts
+++ integration/mover.test.ts
@@ -1,16 +1,16 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { Contract, Signer } from 'ethers';
+import { OracleFactory,OracleFactory__factory } from "../typechain";
 
 describe('Mover', function () {
   let mover: Contract, nft: Contract;
   let alice: Signer, bob: Signer;
   it("test passing", async () => {
         expect(2).eq(2);
-        const m = await ethers.getContractFactory('OracleFactory');
-        const oracle = await m.deploy();
-        await oracle.deployed();
-        const dkgAddr = await oracle.startNewOracle();
+        const [owner, _] = await ethers.getSigners();
+        let factory = await new OracleFactory__factory(owner).deploy();
+        const dkgAddr = await factory.startNewOracle();
         expect(dkgAddr).to.not.be.eq("0")
   });
 })
