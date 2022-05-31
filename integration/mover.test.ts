990a8a2e4c485a24ffde2629d84b303b4bf8472e
nikkolasg <nikkolasg@gmail.com>
2022-05-31 20:13:47 +0100

    hgamal working

--- integration/mover.test.ts
+++ integration/mover.test.ts
@@ -2,15 +2,15 @@ import { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { Contract, Signer } from 'ethers';
 import { OracleFactory,OracleFactory__factory } from "../typechain";
+import { TestContract, TestContract__factory } from "../typechain";
 
-describe('Mover', function () {
+describe('Test Input', function () {
   let mover: Contract, nft: Contract;
   let alice: Signer, bob: Signer;
-  it("test passing", async () => {
+  it("giving inputs", async () => {
         expect(2).eq(2);
         const [owner, _] = await ethers.getSigners();
-        let factory = await new OracleFactory__factory(owner).deploy();
-        const dkgAddr = await factory.startNewOracle();
-        expect(dkgAddr).to.not.be.eq("0")
+        let contract = await new TestContract__factory(owner).deploy();
+        
   });
 })
