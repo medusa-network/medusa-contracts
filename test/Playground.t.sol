ac70b732b722c6e5aadf8708ba01788035dd31db
nikkolasg <nikkolasg@gmail.com>
2022-10-23 12:20:19 +0200

    tests passing

--- test/Playground.t.sol
+++ test/Playground.t.sol
@@ -5,7 +5,6 @@ import "ds-test/test.sol";
 
 import {Playground} from "../src/Playground.sol";
 import {Bn128, G1Point, DleqProof} from "../src/Bn128.sol";
-import {Dleq} from "../src/DleqBN128.sol";
 
 contract PlaygroundTest is DSTest {
     Playground private client;
@@ -16,14 +15,6 @@ contract PlaygroundTest is DSTest {
 
     function testDleq() public {
         assertTrue(true);
-        //assertTrue(
-        //    !client.verifyDLEQProof(
-        //        Bn128.g1(),
-        //        Bn128.g1(),
-        //        DleqProof(1, 2),
-        //        "mylabel"
-        //    )
-        //);
     }
 
     function testShaThis() public {
