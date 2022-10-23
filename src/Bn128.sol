e536ad7e4480eedf2d72f7b276ebc30791230937
nikkolasg <nikkolasg@gmail.com>
2022-10-23 14:22:22 +0200

    enabling dleq verification in oracle

--- src/Bn128.sol
+++ src/Bn128.sol
@@ -24,7 +24,8 @@ library Bn128 {
 
     // p is a prime over which we form a basic field
     // Taken from go-ethereum/crypto/bn256/cloudflare/constants.go
-    uint256 internal constant p = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+    uint256 internal constant p =
+        21888242871839275222246405745257275088696311157297823662689037894645226208583;
 
     /// @dev Gets generator of G1 group.
     ///      Taken from go-ethereum/crypto/bn256/cloudflare/curve.go
@@ -34,13 +35,21 @@ library Bn128 {
     //// --------------------
     ////       DLEQ PART
     //// --------------------
-    uint256 internal constant base2x = 5671920232091439599101938152932944148754342563866262832106763099907508111378;
-    uint256 internal constant base2y = 2648212145371980650762357218546059709774557459353804686023280323276775278879;
-    uint256 internal constant r = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
+    uint256 internal constant base2x =
+        5671920232091439599101938152932944148754342563866262832106763099907508111378;
+    uint256 internal constant base2y =
+        2648212145371980650762357218546059709774557459353804686023280323276775278879;
+    uint256 internal constant r =
+        21888242871839275222246405745257275088548364400416034343698204186575808495617;
 
     /// TODO XXX Can't extract that in its own library because then can't instantiate in Typescript correctly
     /// Seems like a linked library problem with typechain.
-    function dleqverify(G1Point calldata _rg1, G1Point calldata _rg2, DleqProof calldata _proof, uint256 _label)
+    function dleqverify(
+        G1Point calldata _rg1,
+        G1Point calldata _rg2,
+        DleqProof calldata _proof,
+        uint256 _label
+    )
         internal
         view
         returns (
@@ -49,15 +58,30 @@ library Bn128 {
         )
     {
         // w1 = f*G1 + rG1 * e
-        G1Point memory w1 = g1Add(scalarMultiply(g1(), _proof.f), scalarMultiply(_rg1, _proof.e));
+        G1Point memory w1 = g1Add(
+            scalarMultiply(g1(), _proof.f),
+            scalarMultiply(_rg1, _proof.e)
+        );
         // w2 = f*G2 + rG2 * e
-        G1Point memory w2 = g1Add(scalarMultiply(G1Point(base2x, base2y), _proof.f), scalarMultiply(_rg2, _proof.e));
-        uint256 challenge =
-            uint256(sha256(abi.encodePacked(_label, _rg1.x, _rg1.y, _rg2.x, _rg2.y, w1.x, w1.y, w2.x, w2.y))) % r;
-        //return _proof.f;
-        //return scalarMultiply(g1(), _proof.f);
-        //return w2;
-        //return challenge;
+        G1Point memory w2 = g1Add(
+            scalarMultiply(G1Point(base2x, base2y), _proof.f),
+            scalarMultiply(_rg2, _proof.e)
+        );
+        uint256 challenge = uint256(
+            sha256(
+                abi.encodePacked(
+                    _label,
+                    _rg1.x,
+                    _rg1.y,
+                    _rg2.x,
+                    _rg2.y,
+                    w1.x,
+                    w1.y,
+                    w2.x,
+                    w2.y
+                )
+            )
+        ) % r;
         if (challenge == _proof.e) {
             return true;
         }
@@ -84,7 +108,10 @@ library Bn128 {
                 y = p - y;
             }
 
-            require(isG1PointOnCurve(G1Point(x, y)), "Malformed bn256.G1 point.");
+            require(
+                isG1PointOnCurve(G1Point(x, y)),
+                "Malformed bn256.G1 point."
+            );
 
             return G1Point(x, y);
         }
@@ -94,7 +121,11 @@ library Bn128 {
     ///      Byzantium. The result of a point from G1 multiplied by a scalar
     ///      should match the point added to itself the same number of times.
     ///      Revert if the provided point isn't on the curve.
-    function scalarMultiply(G1Point memory p_1, uint256 scalar) internal view returns (G1Point memory p_2) {
+    function scalarMultiply(G1Point memory p_1, uint256 scalar)
+        internal
+        view
+        returns (G1Point memory p_2)
+    {
         // 0x07     id of the bn256ScalarMul precompile
         // 0        number of ether to transfer
         // 96       size of call parameters, i.e. 96 bytes total (256 bit for x, 256 bit for y, 256 bit for scalar)
@@ -105,14 +136,20 @@ library Bn128 {
             mstore(add(arg, 0x20), mload(add(p_1, 0x20)))
             mstore(add(arg, 0x40), scalar)
             // 0x07 is the ECMUL precompile address
-            if iszero(staticcall(not(0), 0x07, arg, 0x60, p_2, 0x40)) { revert(0, 0) }
+            if iszero(staticcall(not(0), 0x07, arg, 0x60, p_2, 0x40)) {
+                revert(0, 0)
+            }
         }
     }
 
     /// @dev Wraps the point addition pre-compile introduced in Byzantium.
     ///      Returns the sum of two points on G1. Revert if the provided points
     ///      are not on the curve.
-    function g1Add(G1Point memory a, G1Point memory b) internal view returns (G1Point memory c) {
+    function g1Add(G1Point memory a, G1Point memory b)
+        internal
+        view
+        returns (G1Point memory c)
+    {
         assembly {
             let arg := mload(0x40)
             mstore(arg, mload(a))
@@ -120,12 +157,18 @@ library Bn128 {
             mstore(add(arg, 0x40), mload(b))
             mstore(add(arg, 0x60), mload(add(b, 0x20)))
             // 0x60 is the ECADD precompile address
-            if iszero(staticcall(not(0), 0x06, arg, 0x80, c, 0x40)) { revert(0, 0) }
+            if iszero(staticcall(not(0), 0x06, arg, 0x80, c, 0x40)) {
+                revert(0, 0)
+            }
         }
     }
 
     /// @dev Returns true if G1 point is on the curve.
-    function isG1PointOnCurve(G1Point memory point) internal view returns (bool) {
+    function isG1PointOnCurve(G1Point memory point)
+        internal
+        view
+        returns (bool)
+    {
         return point.y.modExp(2, p) == (point.x.modExp(3, p) + 3) % p;
     }
 
@@ -171,7 +214,11 @@ library Bn128 {
 library ModUtils {
     /// @dev Wraps the modular exponent pre-compile introduced in Byzantium.
     ///      Returns base^exponent mod p.
-    function modExp(uint256 base, uint256 exponent, uint256 p) internal view returns (uint256 o) {
+    function modExp(
+        uint256 base,
+        uint256 exponent,
+        uint256 p
+    ) internal view returns (uint256 o) {
         assembly {
             // Args for the precompile: [<length_of_BASE> <length_of_EXPONENT>
             // <length_of_MODULUS> <BASE> <EXPONENT> <MODULUS>]
@@ -185,7 +232,9 @@ library ModUtils {
             mstore(add(args, 0xa0), p)
 
             // 0x05 is the modular exponent contract address
-            if iszero(staticcall(not(0), 0x05, args, 0xc0, output, 0x20)) { revert(0, 0) }
+            if iszero(staticcall(not(0), 0x05, args, 0xc0, output, 0x20)) {
+                revert(0, 0)
+            }
             o := mload(output)
         }
     }
@@ -246,7 +295,7 @@ library ModUtils {
                     return x;
                 }
 
-                gs = modExp(g, uint256(2) ** (r - m - 1), p);
+                gs = modExp(g, uint256(2)**(r - m - 1), p);
                 g = (gs * gs) % p;
                 x = (x * gs) % p;
                 b = (b * g) % p;
