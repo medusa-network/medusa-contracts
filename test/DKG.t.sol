906fd419cf9ba20bc08bd4454b66ed9f59b9bb25
Jonathan Easterman <jonathaneasterman@gmail.com>
2023-04-19 10:33:58 +0100

    update licenses in test files

--- test/DKG.t.sol
+++ test/DKG.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT AND Apache-2.0
 pragma solidity ^0.8.19;
 
 import {DealBundle, IDKG, DKG, NotAuthorized, NotRegistered, AlreadyRegistered, ParticipantLimit, InvalidPhase, InvalidSharesCount, InvalidCommitmentsCount} from "../src/DKG.sol";
